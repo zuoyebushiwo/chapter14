bf9552ad59f045ca4dd80de4b82e2aa331fda4d6
zhangxuej <957168943@qq.com>
2015-02-11 14:52:37 +0800

    Change-Id: I87b0c838a5c34a0ece1d9e6d144af33108380b9f

--- src/com/baobaotao/domain/LoginLog.java
+++ src/com/baobaotao/domain/LoginLog.java
@@ -3,44 +3,36 @@ package com.baobaotao.domain;
 import java.io.Serializable;
 import java.util.Date;
 
-@SuppressWarnings("serial")
 public class LoginLog implements Serializable {
-
 	private int loginLogId;
 	private int userId;
 	private String ip;
 	private Date loginDate;
-
+	
 	public int getLoginLogId() {
 		return loginLogId;
 	}
-
 	public void setLoginLogId(int loginLogId) {
 		this.loginLogId = loginLogId;
 	}
-
 	public int getUserId() {
 		return userId;
 	}
-
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
-
 	public String getIp() {
 		return ip;
 	}
-
 	public void setIp(String ip) {
 		this.ip = ip;
 	}
-
 	public Date getLoginDate() {
 		return loginDate;
 	}
-
 	public void setLoginDate(Date loginDate) {
 		this.loginDate = loginDate;
 	}
+	
 
 }
